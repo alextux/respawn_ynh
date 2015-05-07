c5f0b1b523edbd969e14d225c84b39181960acac
alextux <alexan.andrieux@gmail.com>
2015-05-07 16:10:06 +0200

    Create options.php

---
+++ conf/options.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * DATADIR is defined in index.php, and cannot be defined elsewhere
+ *
+$GLOBALS['config']['DATADIR'] = '/home/yunohost.app/respawn/data'; // Data subdirectory
+*/
+$GLOBALS['config']['CONFIG_FILE'] = $GLOBALS['config']['DATADIR'].'/config.php'; // Configuration file (user login/password)
+?>
